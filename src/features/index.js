8b1588c3cb77b2fe660a0f5b63d7cf9006a7436b
John Raptis <johnsitpar@gmail.com>
2023-11-25 13:38:23 +0200

    first commit

--- src/features/index.js
+++ src/features/index.js
@@ -1,10 +1,10 @@
 ////
 ///
 //
-// This folder is where each feature lives in isolation to the rest of the app exepct that 
+// This folder is where each feature lives in isolation to the rest of the app except that 
 // it uses parts of the ui folder.
 //
-// Each feature has its own components and hooks folder specific to the feature. 
+// Each feature has its own components and hooks folder, specific to the feature. 
 // If the feature is super involved it can have its own utils and services folders as well.
 //
 // This strategy has the huge benefit that it makes it very easy to remove features
